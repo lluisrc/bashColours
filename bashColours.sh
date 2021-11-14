8267b6f3f5ddef569d9b9f343e21d67108d0acfa
lluisrc <60383607+lluisrc@users.noreply.github.com>
2021-11-14 16:51:16 +0100

    Add files via upload

---
+++ bashColours.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+endColour="\e[0m"
+redColour="\e[0;31m"
+greenColour="\e[0;32m"
+yellowColour="\e[0;33m"
+blueColour="\e[0;34m"
+purpleColour="\e[0;35m"
+cianColour="\e[0;36m"
+greyColour="\e[0;37m"
+
+echo -e "${redColour}Rojo${endColour}"
+echo -e "${greenColour}Verde${endColour}"
+echo -e "${yellowColour}Amarillo${endColour}"
+echo -e "${blueColour}Azul${endColour}"
+echo -e "${purpleColour}Violeta${endColour}"
+echo -e "${cianColour}Cian${endColour}"
+echo -e "${greyColour}Gris${endColour}"
